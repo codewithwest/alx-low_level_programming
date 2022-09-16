a04fea943c622a53470a443607a6b7c28c1c40be
codewithwest <jonaslekgau@gmail.com>
2022-09-15 18:11:04 -0700

    debuggd

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,2 @@
+# alx-low_level_programming
+Lets debugg this things
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,40 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+void pr_digit(int charcode, int fmt);
+int add(int a, int b);
+void pr_with_fmt(int charcode, int fmt);
+void print_to_98(int n);
+void pr_3_digit(int charcode, int fmt);
+void print_times_table(int n);
+void print_spaces(int n);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_numbers(void);
+void print_zero(void);
+
+#endif
